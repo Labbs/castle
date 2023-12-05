1ed3cf859a86b7b108ac79abf76c68eea6d7bd88
Jeremy Mouton <moutonjeremy@labbs.fr>
2023-12-05 14:19:25 +0100

    fix rows name

--- modules/repository/domain/repository.go
+++ modules/repository/domain/repository.go
@@ -3,17 +3,17 @@ package domain
 import "time"
 
 type Repository struct {
-	Id          string `gorm:"primaryKey" json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description,omitempty"`
-	Type        string `json:"type,omitempty"`
-	Url         string `json:"url,omitempty"`
-	SSHKey      string `json:"ssh_key,omitempty"`
-	SSHKeyPass  string `json:"ssh_key_pass,omitempty"`
+	Id               string `gorm:"primaryKey" json:"id"`
+	Name             string `json:"name"`
+	Description      string `json:"description,omitempty"`
+	Type             string `json:"type,omitempty"`
+	Url              string `json:"url,omitempty"`
+	SSHKey           string `json:"ssh_key,omitempty"`
+	SSHKeyPassphrase string `json:"ssh_key_passphrase,omitempty"`
 
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	DeleteAt  time.Time `json:"-"`
+	DeletedAt time.Time `json:"-"`
 }
 
 func (r *Repository) TableName() string {
