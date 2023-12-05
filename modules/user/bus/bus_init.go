81e6e4d2f1f39bbeb8c9d32e93b3b10607bf984e
Jeremy Mouton <moutonjeremy@labbs.fr>
2023-12-05 11:29:20 +0100

    change function name, add debug logs, add error to return response

--- modules/user/bus/bus_init.go
+++ modules/user/bus/bus_init.go
@@ -3,17 +3,20 @@ package bus
 import (
 	"github.com/labbs/castle/modules/user/bootstrap"
 	"github.com/labbs/castle/modules/user/repository"
+	"github.com/rs/zerolog"
 )
 
 type UserController struct {
 	Repository repository.UserRepository
+	Logger     zerolog.Logger
 }
 
 func Setup(app bootstrap.Application) {
 	ur := repository.NewUserRepository(app.Db)
 	uc := &UserController{
 		Repository: ur,
+		Logger:     app.Logger,
 	}
 
-	app.Bus.AddHandler("user:get_by_username", uc.GetByUsername)
+	app.Bus.AddHandler("user:get_by_email", uc.GetByEmail)
 }
