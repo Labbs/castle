2a1a3705e3846fd7fd21bbfad53c0cf4708a0be4
Mouton Jeremy <moutonjeremy@labbs.fr>
2024-01-03 13:27:15 +0100

    update scheduler bus

--- modules/scheduler/bus/bus_init.go
+++ modules/scheduler/bus/bus_init.go
@@ -2,18 +2,26 @@ package bus
 
 import (
 	"github.com/labbs/castle/modules/scheduler/bootstrap"
+	"github.com/labbs/castle/modules/scheduler/domain"
 	"github.com/labbs/castle/modules/scheduler/repository"
+	"github.com/labbs/castle/modules/scheduler/service"
+	"github.com/rs/zerolog"
 )
 
-type Controller struct {
-	Repository repository.CronRepository
+type SchedulerController struct {
+	Service domain.SchedulerService
+	Logger  zerolog.Logger
 }
 
 func Setup(app bootstrap.Application) {
-	ur := repository.NewCronRepository(app.CronScheduler)
-	uc := &Controller{
-		Repository: ur,
+	ur := repository.NewSchedulerRepository(app.CronScheduler, app.Logger)
+	uc := &SchedulerController{
+		Service: service.NewSchedulerService(ur),
+		Logger:  app.Logger,
 	}
 
 	app.Bus.AddHandler("scheduler:cron:add", uc.AddSchedulerCronJob)
+	app.Bus.AddHandler("scheduler:cron:remove", uc.RemoveSchedulerCronJob)
+	app.Bus.AddHandler("scheduler:cron:get_all", uc.GetAllSchedulerCronJobs)
+	app.Bus.AddHandler("scheduler:cron:run", uc.RunSchedulerCronJob)
 }
--- modules/scheduler/bus/handler.go
+++ modules/scheduler/bus/handler.go
@@ -1,6 +1,51 @@
 package bus
 
+import (
+	"github.com/goccy/go-json"
+
+	"github.com/labbs/castle/modules/scheduler/domain"
+)
+
 // Bus handler for scheduler:cron:add
-func (uc *Controller) AddSchedulerCronJob(data interface{}) interface{} {
-	return nil
+func (uc *SchedulerController) AddSchedulerCronJob(data interface{}) (interface{}, error) {
+	var task domain.Task
+	err := json.Unmarshal(data.([]byte), &task)
+	if err != nil {
+		return map[string]string{}, err
+	}
+
+	err = uc.Service.AddSchedulerCronJob(task)
+	if err != nil {
+		return map[string]string{}, err
+	}
+	return map[string]string{"success": "scheduler cron job added"}, nil
+}
+
+// Bus handler for scheduler:cron:remove
+func (uc *SchedulerController) RemoveSchedulerCronJob(data interface{}) (interface{}, error) {
+	jobId := data.(string)
+	err := uc.Service.RemoveSchedulerCronJob(jobId)
+	if err != nil {
+		return map[string]string{}, err
+	}
+	return map[string]string{"success": "scheduler cron job removed"}, nil
+}
+
+// Bus handler for scheduler:cron:get_all
+func (uc *SchedulerController) GetAllSchedulerCronJobs(data interface{}) (interface{}, error) {
+	jobs, err := uc.Service.GetAllSchedulerCronJobs()
+	if err != nil {
+		return map[string]string{}, err
+	}
+
+	j, err := json.Marshal(jobs)
+	if err != nil {
+		return map[string]string{}, err
+	}
+	return j, nil
+}
+
+// Bus handler for scheduler:cron:run
+func (uc *SchedulerController) RunSchedulerCronJob(data interface{}) (interface{}, error) {
+	return nil, nil
 }
